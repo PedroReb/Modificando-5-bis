2785a8ca611e22980b1ca1ca69e885c732cd9f77
PedroReb <148907515+PedroReb@users.noreply.github.com>
2024-01-31 19:31:48 +0100

    Add files via upload

---
+++ Sergioteacher.Csharp04.Inputbox/Sergioteacher.Csharp04.Inputbox/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sergioteacher.Csharp04.Inputbox
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public WindowInput ventanaInput;
+        public string data;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ventanaInput = new WindowInput(this);
+
+
+        }
+
+        private void BIntro_Click(object sender, RoutedEventArgs e)
+        {
+            ventanaInput.Show();
+            this.Hide();
+        }
+
+        private void VentanaPrincipal_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Application.Current.Shutdown();
+        }
+
+        private void VentanaPrincipal_Activated(object sender, EventArgs e)
+        {
+            Tsalida.Text = data;
+        }
+
+        private void BRef_Click(object sender, RoutedEventArgs e)
+
+        {
+            string[] files = Directory.GetFiles(Tsalida.Text);
+            foreach (string i in files)
+            {
+                addListItem(i);
+            }
+            Talm.Text = data;
+        }
+
+        private void addListItem(string i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
